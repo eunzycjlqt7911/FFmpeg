4d93f0d7a3f167f0190ac5ddeab5243aed1fd374
kukk <kukk@q.com>
2025-06-10 19:37:02 +0800

    2

--- libavformat/rtmpproto.c
+++ libavformat/rtmpproto.c
@@ -2727,14 +2727,8 @@ static int rtmp_open(URLContext *s, const char *uri, int flags, AVDictionary **o
                     proxy_host, sizeof(proxy_host), &proxy_port,
                     NULL, 0, rt->http_proxy);
 
-        // For proxy connections, we need to pass the full target host:port with TCP parameters
-        if (rt->listen)
-            ff_url_join(dest, sizeof(dest), "tcp", NULL, hostname, port,
-                        "?listen&listen_timeout=%d&tcp_nodelay=%d",
-                        rt->listen_timeout * 1000, rt->tcp_nodelay);
-        else
-            ff_url_join(dest, sizeof(dest), "tcp", NULL, hostname, port,
-                        "?tcp_nodelay=%d", rt->tcp_nodelay);
+        // For proxy connections, we need to pass the target host:port
+        ff_url_join(dest, sizeof(dest), NULL, NULL, hostname, port, NULL);
         
         // Use httpproxy protocol for HTTP proxy tunneling
         ff_url_join(buf, sizeof(buf), "httpproxy", proxy_auth, proxy_host,
